b228d7237a1741c526ba64e2b0366c8f69e77f5b
oddegen <nahomtensae@gmail.com>
2025-05-07 05:02:51 +0300

    ops: frontend depends on envoy and backend service

--- backend/worker.php
+++ backend/worker.php
@@ -6,11 +6,10 @@ use Spiral\Goridge\StreamRelay;
 use Spiral\GRPC\Server;
 use Spiral\RoadRunner\Worker;
 
-ini_set('display_errors', 'stderr');
 require __DIR__ . '/vendor/autoload.php';
 
 $server = new Server(null, [
-    'debug' => true, // optional (default: false)
+    'debug' => false,
 ]);
 
 $server->registerService(EchoInterface::class, new EchoService());
